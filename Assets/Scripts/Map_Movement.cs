7efe7134e65231c9a5a0c82f84dda8b2b9125ff0
André Felipe de Barros A. N <afbarrosan@gmail.com>
2023-06-05 18:30:53 -0300

    Revert "Beta Version 0.0.2 🚀🌌"
    This reverts commit 6c2b595d8219f5f2f04926279c181d081560cf0e.

--- Assets/Scripts/Map_Movement.cs
+++ Assets/Scripts/Map_Movement.cs
@@ -5,25 +5,24 @@ using UnityEngine;
 public class Map_Movement : MonoBehaviour
 {
     public GameObject map;
-    private Transform mapTransform;
+    private Rigidbody rb;
     public Vector3 moveDirection = Vector3.right;
-    [SerializeField] private float speed;
-
+    [SerializeField]private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        mapTransform = map.transform;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Calcula o vetor de movimento com base na direção e velocidade especificadas
-        Vector3 movement = moveDirection.normalized * speed * Time.deltaTime;
+        Vector3 movement = moveDirection.normalized * speed * Time.fixedDeltaTime;
 
-        // Aplica o movimento ao Transform
-        mapTransform.position += movement;
+        // Aplica o movimento ao Rigidbody
+        rb.MovePosition(rb.position + movement);
     }
 }
 
